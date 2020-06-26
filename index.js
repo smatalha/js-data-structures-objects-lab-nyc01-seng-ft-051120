f87773bd0ec52ea3d844e5f3c809ab597758999c
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-26 00:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
+  const newObj = { ...driver }
   delete newObj[key];
 
   return newObj;
